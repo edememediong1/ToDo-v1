2ea45994d5cf102105c399761f92fb5ce9bfaef9
Emediong Bassey Edem <edememediong1@gmail.com>
2024-05-14 16:35:56 +0100

    Added moveup and movedown functionality

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -21,11 +21,20 @@ export default function ToDoLIst(){
     }
 
     function moveTaskUp(index){
+        if (index > 0){
+            const updatedTasks = [...tasks];
+            [updatedTasks[index], updatedTasks[index-1]] = [updatedTasks[index - 1], updatedTasks[index]];
+            setTasks(updatedTasks);
+        }
 
     }
     
     function moveTaskDown(index){
-
+        if (index < tasks.length - 1){
+            const updatedTasks = [...tasks];
+            [updatedTasks[index], updatedTasks[index + 1]] = [updatedTasks[index + 1], updatedTasks[index]];
+            setTasks(updatedTasks);
+        }
     }
 
     return(
